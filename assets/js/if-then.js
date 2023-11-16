e8e8741cb07f326f8bae8daae16753a44e30dbd7
elop123 <142408289+elop123@users.noreply.github.com>
2023-11-16 11:14:44 +0100

    Update if-then.js

--- assets/js/if-then.js
+++ assets/js/if-then.js
@@ -8,29 +8,52 @@ const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B.
 
-if ( /*..dit expression Her*/ ) {
+if ( A!==B ) {
     console.log('statement true!');
+} else if (A=B){
+    console.log('expression is false');
 }
 
 
 //opgave 2: skrive en If then statement der vurderer om A er større end B.
+if (A>B) {
+    console.log('expression is false!');
+ } else {
+        console.log('expression is true!');
+    }
 
 
 
 
 //opgave 3: Skriv en If then statement der vurderer om A er mindre end eller lig med B
-
+if (A<=B){
+    console.log('statement is true');
+} else{
+    console.log('statement is false');
+}
 
 
 
 
 //opgave 4: Skriv en If then statement der bliver FALSE når du sammenligner B og C.
 
+if (B === C){
+    console.log('statement is true');
+}
+    else{
+        console.log('statement is false');
+    }
 
 
 
 
 //opgave 5: Skriv en If then statement der bliver TRUE når du sammenligner B og C.
+if (B !== C){
+    console.log('statement is true');
+}
+    else{
+        console.log('statement is false');
+    }
 
 
 
